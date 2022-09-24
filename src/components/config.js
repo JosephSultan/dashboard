6dbf3f1d60502b6444aaa10447d6a828e2db9079
JosephSultan <105505623+JosephSultan@users.noreply.github.com>
2022-09-24 14:19:25 +0200

    Update config.js

--- src/components/config.js
+++ src/components/config.js
@@ -1,5 +1,5 @@
 
 export default{
-  apiKey:'AIzaSyCFmIB7fkHTawGHPL_f657pVZSvY1aDRhM',
+  apiKey:'AIzaSyAy2JBwR7Ua0InR1i2Fu2ZV8yidDHQucEo',
   spreadsheetId:'1zdHQOsjioElXgIy02gnv4Hff2pEijLyJcWBAFAPPA8Y'
 }
